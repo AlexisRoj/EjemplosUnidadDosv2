f1fbde8b3c7b0e5ec920db1764b405a28c3cc865
AlexisRoj <alexisroj2@gmail.com>
2016-09-21 09:29:38 -0600

    Se implementa Widget

---
+++ app/src/main/java/layout/ImageWidgetClass.java
@@ -0,0 +1,16 @@
+package layout;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by Alexis on 21/9/2016.
+ */
+public class ImageWidgetClass extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        /** Este metodo asegura que el Widget este disponible*/
+    }
+}
